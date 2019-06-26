2c449935940015854d84777c5c3e206fe00c21fb
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-26 16:20:59 -0300

    Update AR (1).R

--- AR (1).R
+++ AR (1).R
@@ -1,19 +1,23 @@
-# Packages
+# Author: Werley Cordeiro
+# werleycordeiro@gmail.com
+
+# Package
 
 install.packages("quantmod")
+
 library(quantmod)
 
 # Data
 
 getSymbols('PBR',return.class = 'xts',index.class = 'Date',from = "2019-01-01",to = Sys.Date(),periodicity = "daily",src='yahoo')
 
-data = PBR$PBR.Close
+data = PBR$PBR.Close # Petróleo Brasileiro S.A. - Petrobras (PBR) - NYSE (USD)
 
 # AR(p)
 
 # OLS
 
-p = 1
+p = 2
 T = dim(data)[1]
 
 if(p==1){
@@ -26,9 +30,9 @@ if(p==1){
 			for(i in 1:p){
 	 			if(i<p){
 					 aux = as.numeric(data[1:(T-i)]) # exclude final observations according to order p
-	 			  X[,i]= aux[-(1:(p-i))] # exclude initials observations according to order p
+	 			  X[,i]= aux[-(1:(p-i))] # exclude initial observations according to order p
 				}else{
-					X[,i] = as.numeric(data[1:(T-p)]) # exclude only final observations in the last vector: p
+					X[,i] = as.numeric(data[1:(T-p)]) # exclude only final observations in the last vector p
 			}
 		}
 		Y = as.numeric(data[-(1:p)])
@@ -41,6 +45,5 @@ b = solve(t(X) %*% X) %*% t(X) %*% Y
 pars = expand.grid(orderp = 1:p)
 names = paste0("Beta",pars$orderp)
 rownames(b) = c("intercept",names)
-
 b
 
