b46b632de6581e62e3a3d7dd296d6dc21a5f5822
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-26 16:56:10 -0300

    Create ar_p.R

---
+++ ar_p.R
@@ -0,0 +1,31 @@
+# OLS
+# AR(p)
+
+ar_p<-function(data,p){
+T = dim(data)[1]
+if(p==1){
+	Y = as.numeric(data[-1])
+	X = as.numeric(data[-T])
+	X = cbind(rep(1,length(X)),X)
+}else{
+	if(p>1){
+		X = matrix(NA,T-p,p)
+			for(i in 1:p){
+	 			if(i<p){
+					 aux = as.numeric(data[1:(T-i)]) # exclude final observations according to order p
+	 			  X[,i]= aux[-(1:(p-i))] # exclude initial observations according to order p
+				}else{
+					X[,i] = as.numeric(data[1:(T-p)]) # exclude only final observations in the last vector p
+			}
+		}
+		Y = as.numeric(data[-(1:p)])
+		X = cbind(rep(1,dim(X)[1]),X)
+		}
+}
+
+b = solve(t(X) %*% X) %*% t(X) %*% Y
+pars = expand.grid(orderp = 1:p)
+names = paste0("Beta",pars$orderp)
+rownames(b) = c("intercept",names)
+return(b)
+}
