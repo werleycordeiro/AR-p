99d0d3a9af290ce5b1dd298cb54069941cfbdc35
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-29 07:14:46 -0300

    Update ar_p.R

--- ar_p.R
+++ ar_p.R
@@ -8,7 +8,9 @@ dat = dataf(data=data,inter=inter,p=p)
 Y = dat$Y
 X = dat$X
 
-b = solve(t(X) %*% X) %*% t(X) %*% Y
+source("ols.R")
+b = ols(X=dat$X,Y=dat$Y)
+
 pars = expand.grid(orderp = 1:p)
 names = paste0("Beta",pars$orderp)
 if(inter){rownames(b) = c("intercept",names)}else{rownames(b) = c(names)}
